64c3a2b4ea8dbad77a5ae80716289529ca144622
xuchenzhe <904429845@qq.com>
2023-01-13 18:07:18 +0800

    增加多线程

--- demo.py
+++ demo.py
@@ -1,15 +1,39 @@
+import threading
+import time
+
 import pandas as pd
 
 import fakedata as fd
 
-if __name__ == '__main__':
+
+def gen_person(filename, num):
+    # 运行时间计算
+    locT = time.time()
     person_list = []
     # 在这里可以调整数据生成的总数
-    for i in range(0, 10000):
+    for i in range(0, num):
         person_list.append(fd.new_person().split(","))
 
     # 数据有三列，列名分别为one,two,three
-    name = ['id', 'name', 'gender', 'province', 'city', 'area']
+    name = ['idcard', 'name', 'gender', 'ethnic', 'province', 'city', 'area']
     test = pd.DataFrame(columns=name, data=person_list)
-    print(test)
-    test.to_csv('cvs/A3.csv',index=False)
+    test.to_csv('cvs/{}.csv'.format(filename), index=False)
+    return 0
+
+
+if __name__ == '__main__':
+    new_thread1 = threading.Thread(target=gen_person('T1', 10000), name='T1')
+    new_thread2 = threading.Thread(target=gen_person('T2', 10000), name='T2')
+    new_thread3 = threading.Thread(target=gen_person('T3', 10000), name='T3')
+    new_thread4 = threading.Thread(target=gen_person('T4', 10000), name='T4')
+    new_thread5 = threading.Thread(target=gen_person('T5', 10000), name='T5')
+
+    new_thread1.start()
+    new_thread2.start()
+    new_thread3.start()
+    new_thread4.start()
+    new_thread5.start()
+
+    print("当前线程数量为", threading.active_count())
+    print("所有线程的具体信息", threading.enumerate())
+    print("当前线程具体信息", threading.current_thread())
--- test.py
+++ test.py
@@ -2,10 +2,12 @@ import time
 
 
 def function(n):
-    for i in range(0,n):
+    for i in range(0, n):
         time.sleep(1)
         print(i)
 
 
 if __name__ == '__main__':
-    function(10)
+    t = time.gmtime()
+    time.sleep(3)
+
